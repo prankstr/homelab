e8ed3900a2c6da3eb254328c441f43663dd7d3e6
prankstr <11200347+prankstr@users.noreply.github.com>
2024-07-06 15:19:44 +0200

    Add compose down before up

--- kubernetes/talos/updater.sh
+++ kubernetes/talos/updater.sh
@@ -3,10 +3,11 @@
 REMOTE=https://raw.githubusercontent.com/prankstr/homelab/main/kubernetes/talos/docker-compose.yaml
 
 if curl -s $REMOTE | cmp -s docker-compose.yaml; then
-	# No diff
-	exit 0
+  # No diff
+  exit 0
 fi
 
 curl -O $REMOTE
 
+docker compose down
 docker compose up -d
