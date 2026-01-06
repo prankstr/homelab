6c26e94559d2847f20a60939f665f7263dacd715
David <11200347+prankstr@users.noreply.github.com>
2026-01-06 17:40:11 +0100

    Add new required sqlite flag to omni compose file. Update updater script

--- kubernetes/talos/updater.sh
+++ kubernetes/talos/updater.sh
@@ -2,12 +2,11 @@
 
 REMOTE=https://raw.githubusercontent.com/prankstr/homelab/main/kubernetes/talos/docker-compose.yaml
 
-if curl -s $REMOTE | cmp -s docker-compose.yaml; then
-  # No diff
-  exit 0
+if curl -s $REMOTE | cmp -s docker-compose-remote.yaml; then
+    exit 0
 fi
 
-curl -O $REMOTE
+curl -o docker-compose-remote.yaml $REMOTE
+sed 's/${MY_DOMAIN}/<actual domain>/g' docker-compose-remote.yaml > docker-compose.yaml
 
-docker compose down
 docker compose up -d
